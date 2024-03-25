d9a8e417e3560bf6880f2f8175ff0c3615eb433a
Boris Lukačovič <xlukacov@fi.muni.cz>
2024-03-25 22:19:19 +0100

    Add: Tests and test data

---
+++ TennisReservationSystem/src/main/java/inqool/thingy/tennisreservationsystem/data/TestData.java
@@ -0,0 +1,59 @@
+package inqool.thingy.tennisreservationsystem.data;
+
+import inqool.thingy.tennisreservationsystem.api.model.CourtType;
+import inqool.thingy.tennisreservationsystem.api.model.TennisCourt;
+import inqool.thingy.tennisreservationsystem.service.ServiceProvider;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import java.util.Random;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class TestData {
+
+    private static final List<String> COURT_TYPE_NAMES = List.of("Grass", "Clay", "Hard", "Artificial");
+
+    public static void initData() {
+        SessionFactory sessionFactory = ServiceProvider.getSessionFactory();
+
+        List<CourtType> courtTypes = getRandomCourtType(2);
+        List<TennisCourt> tennisCourts = getTestCourts(courtTypes, 4);
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            courtTypes.forEach(session::persist);
+            tennisCourts.forEach(session::persist);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    public static List<TennisCourt> getTestCourts(List<CourtType> courtTypes, int numberOfCourts) {
+        List<TennisCourt> result = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < numberOfCourts; i++) {
+            int index = rand.nextInt(courtTypes.size());
+            result.add(new TennisCourt(courtTypes.get(index)));
+        }
+
+        return result;
+    }
+
+    public static List<CourtType> getRandomCourtType(int numberOfTypes) {
+        List<CourtType> result = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < numberOfTypes; i++) {
+            int index = rand.nextInt(COURT_TYPE_NAMES.size());
+            float hourlyRentPrice = rand.nextFloat(5, 25);
+
+            result.add(new CourtType(i, COURT_TYPE_NAMES.get(index), hourlyRentPrice));
+        }
+
+        return result;
+    }
+
+}
--- TennisReservationSystem/src/test/java/inqool/thingy/tennisreservationsystem/TennisReservationSystemApplicationTests.java
+++ TennisReservationSystem/src/test/java/inqool/thingy/tennisreservationsystem/TennisReservationSystemApplicationTests.java
@@ -1,13 +1,30 @@
 package inqool.thingy.tennisreservationsystem;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static io.restassured.RestAssured.post;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TennisReservationSystemApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
 
     @Test
     void contextLoads() {
+        post("/api/user?name=John").then().assertThat()
+                .body("name", equalTo("John"));
     }
 
 }
